25b1b0087f94cf017175378f6738f0927f200e6a
C Venkitakrishnan <venkitakrishnan@itflux.com>
2013-10-24 01:13:35 +0530

    Refactored, reorganized and added mail.js which can be used to send emails using cordys send mail service

--- trunk/js/snips/Util.js
+++
@@ -1,7 +0,0 @@
-function pause(a) {
-    var limit = (Date.now() || (new Date().getTime())) + 1E3 * a;
-    while (limit) {
-        if (limit <= (Date.now() || (new Date().getTime())))
-            break;
-    }
-}
